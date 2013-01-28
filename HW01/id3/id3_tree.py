3ece7f52375b4f83408a2fe9a4077ed26d0c1a5a
David Stevens <david.stevens@webfilings.com>
2013-01-28 06:02:21 -0800

    added chi-sqared test

--- HW01/id3/id3_tree.py
+++ HW01/id3/id3_tree.py
@@ -32,17 +32,20 @@ def expected_info(n,p):
     p_term = p_ratio * np.log2(p_ratio) if p else 0
     return -1 * (n_term + p_term)
 
-def p(depth=0,**a):
-    for k,v in a.items():
-        print('depth:{}\n{}:\n{}\n'.format(depth,k,v))
 def mutual_info(row, classes):
     counts = get_counts(row, classes)
     return sum([(n + p) * expected_info(n, p)
                 for n, p in counts]) / len(row)
 
-def best_split(data):
-    data = data
-    return np.argmin([mutual_info(row, data[-1]) for row in data[:-1]])
+def best_split(data, chi=False):
+    classes = data[-1]
+    data = data [:-1]
+    if chi:
+        filter_ = np.array([chi_squared(row, classes) > chi for row in data[:-1]])
+        data = data[filter_]
+    if len(data) == 0:
+        return -1
+    return np.argmin([mutual_info(row, classes) for row in data])
 
 def split(data, index):
     filter_ = np.array([i != index for i in range(len(data[...,0]))])
@@ -50,36 +53,52 @@ def split(data, index):
     right = data[...,data[index]==1][filter_]
     return left, right
 
-def grow_tree(data_file):
-    util.verify(data_file)
-    data = np.loadtxt(data_file, skiprows=1, delimiter=',').T
-    with open(data_file) as f:
-        headers = f.readline().strip().split(',')
-    return ID3Tree(data, headers)
-
 def mode(array):
     if isinstance(array, np.integer):
         return int(array)
     return int(np.argmax(np.bincount(array.astype(int))))
 
+def chi_squared(row, classes):
+    total = len(row)
+    p = sum(row)
+    n = total - p
+    counts = get_counts(row, classes)
+    chi = 0
+    for count in counts:
+        p_, n_ = (p * count[1] / total), (n * count[0] / total)
+        chi += (count[1] - p_) ** 2 / p_ if p_ else 0
+        chi += (count[0] - n_) ** 2 / n_ if n_ else 0
+    return chi
+
+def grow_tree(data_file, chi=False):
+    util.verify(data_file)
+    data = np.loadtxt(data_file, skiprows=1, delimiter=',').T
+    with open(data_file) as f:
+        headers = f.readline().strip().split(',')
+    return ID3Tree(data, headers, chi=chi)
+
+
 class ID3Tree(tree.Tree):
     
-    def __init__(self, data, headers, depth=0):
+    def __init__(self, data, headers, depth=0, chi=False):
         self.headers = headers
-        best_attr = best_split(data)
-        super().__init__(headers[best_attr], depth)
-        left_data, right_data = split(data, best_attr)
-        new_headers = self.headers[:best_attr] + self.headers[best_attr+1:]
-        if left_data.size != 0:
-            if left_data.ndim == 1 or left_data.shape[0] == 1:
-                self.left = mode(left_data[-1])
-            else:
-                self.left = ID3Tree(left_data, new_headers, self.depth+1)
-        if right_data.size != 0:
-            if right_data.ndim == 1 or right_data.shape[0] == 1:
-                self.right = mode(right_data[-1])
-            else:
-                self.right = ID3Tree(right_data, new_headers, self.depth+1)
+        best_attr = best_split(data, chi)
+        if best_attr == -1:
+            super().__init__(mode(data[-1]), depth)
+        else:
+            super().__init__(headers[best_attr], depth)
+            left_data, right_data = split(data, best_attr)
+            new_headers = self.headers[:best_attr] + self.headers[best_attr+1:]
+            if left_data.size != 0:
+                if left_data.ndim == 1 or left_data.shape[0] == 1:
+                    self.left = mode(left_data[-1])
+                else:
+                    self.left = ID3Tree(left_data, new_headers, self.depth+1, chi)
+            if right_data.size != 0:
+                if right_data.ndim == 1 or right_data.shape[0] == 1:
+                    self.right = mode(right_data[-1])
+                else:
+                    self.right = ID3Tree(right_data, new_headers, self.depth+1, chi)
     
     def classify(self, values):
         if self.left is None and self.right is None:
--- HW01/id3/main.py
+++ HW01/id3/main.py
@@ -3,16 +3,33 @@ import id3
 import id3.util as util
 import id3.id3_tree
 
+p_table = {'0.10':2.706, '0.05':3.841, '0.025':5.024, '0.01':6.635,
+           '0.001': 10.828, 'Do Not Use Chi-Squared Test':False}
+choices = {'1':'0.10', '2':'0.05', '3':'0.025', '4':'0.01', '5':'0.001',
+           'N':'Do Not Use Chi-Squared Test', 'Q':'Quit'}
 
 def main(training_data_file, test_data_file, model_file):
-    tree = id3.id3_tree.grow_tree(training_data_file)
+    choice = -1
+    print('What p-value would you like for chi-squared test?\n')
+    for k in sorted(choices):
+        print('{}: {:<5}'.format(k, choices[k]))
+    while True:
+        choice = input('\nChoice: ').upper()[0]
+        if choice == 'Q':
+            sys.exit(0)
+        if choice not in choices:
+            print('Invalid choice!')
+        else:
+            break
+    chi = p_table[choices[choice]]
+    tree = id3.id3_tree.grow_tree(training_data_file, chi)
     tree.dump_model(model_file)
     answers = []
     for row, class_ in util.load_test(test_data_file):
         values = {k:v for k,v in zip(tree.headers, row)}
         answers.append(tree.classify(values) == class_)
     accuracy = sum(answers) / len(answers)
-    print('Accuracy: {:.2%}'.format(accuracy))
+    print('\nAccuracy: {:.2%}'.format(accuracy))
     
 if __name__ == '__main__':
     if sys.version_info[0] != 3:
@@ -21,7 +38,7 @@ if __name__ == '__main__':
         sys.exit(1)
     args = sys.argv
     if len(args) != 4:
-        sys.stderr.write('Proper usage:\n\n\tpython id3.py <training_input_file> <test_input_file> <model_output_file>\n\n')
+        sys.stderr.write('Proper usage:\n\n\tpython -m id3.main.py <training_input_file> <test_input_file> <model_output_file>\n\n')
         sys.exit(1)
     for f_name in args[1:3]:
         util.verify(f_name)
